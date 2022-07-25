b0360d827dbc740bc6009a027902e2cb6ee59f1b
sjpark <sjpark>
2022-07-26 08:27:41 +0900

    Initial commit

---
+++ src/main/java/com/shinjung/shop/toimoishop/ToimoiShopApplication.java
@@ -0,0 +1,13 @@
+package com.shinjung.shop.toimoishop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToimoiShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToimoiShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shinjung/shop/toimoishop/ToimoiShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shinjung.shop.toimoishop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToimoiShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
